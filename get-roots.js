650941e336998e0fd6d28ad491b489bd6e7c538d
vinacovre <viniassis@me.com>
2018-07-23 19:22:36 -0300

    add function to get root

---
+++ get-roots.js
@@ -0,0 +1,23 @@
+const _     = require('lodash');
+const graphlib = require('graphlib');
+
+const getRoots = (g, contentType) => {
+  const predecessors = g.predecessors(contentType);
+
+  if (!predecessors) {
+    throw new Error(`There \is \no content type named '${contentType}'`);
+  }
+
+  if (predecessors.length === 0) {
+    return contentType;
+  }
+
+  return predecessors.map(contentType => getRoots(g, contentType));
+}
+
+const flattenRoots = (g, contentType) => {
+  const roots = getRoots(g, contentType);
+  return _.flattenDeep(roots);
+}
+
+module.exports = flattenRoots;
