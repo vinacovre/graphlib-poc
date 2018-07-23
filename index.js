d79dd3d327848c556ab74ab91b3a222106a48159
vinacovre <viniassis@me.com>
2018-07-23 19:41:29 -0300

    update index with new buildGraph function

--- index.js
+++ index.js
@@ -1,33 +1,18 @@
-const content = require('./content.json').content_types;
 const Graph = require('graphlib').Graph;
 const _ = require('lodash');
 const getRoots = require('./get-roots');
+const buildGraph = require('./build-graph');
+
+const content = require('./content.json').content_types;
+
 let g = new Graph();
 
-let all = [];
-content.forEach(contentType => {
-  g.setNode(contentType.uid);
-  const schema = contentType.schema;
-  const references = schema
-    .filter(dataType => dataType.data_type === 'reference')
-    .map(destination => {
-      // console.log(`Seting edge \from ${contentType.uid} to ${destination.uid} `);
-      g.setEdge(contentType.uid, destination.uid);
-      return destination.uid;
-  });
+buildGraph({g, content});
 
-  all.push({
-    contentType: contentType.uid,
-    references
-  });
-});
-console.log(g.inEdges('navbar').length);
-
-const contentType = 'footer';
-const roots = getRoots(g, contentType);
-const flattenRoots = _.flattenDeep(roots);
+const contentType = 'faq';
+const roots = getRoots({g, contentType});
 
 console.log(`\nROOTS OF '${contentType}':`);
-console.log(flattenRoots);
+console.log(roots);
 console.log(`\nROOTS COUNT ('${contentType}'):`);
-console.log(flattenRoots.length);
+console.log(roots.length);
